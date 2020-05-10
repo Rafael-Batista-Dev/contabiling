f5bb7a33781d1e2c2445e7c8976a0dd0c486abb6
unknown <rafaelbatista.sth@gmail.com>
2020-05-10 17:45:32 -0300

    create page portifolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,89 @@
 import React from "react";
+import {
+  PortfolioSection,
+  PortfolioItem,
+  PortfolioTitle,
+  Span,
+  List,
+  Box,
+  Image,
+  Overlay,
+  OverlaySpan,
+} from "./style";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <PortfolioSection>
+      <PortfolioTitle>
+        <Span>Nosso</Span> Portfólio
+      </PortfolioTitle>
+      <List>
+        <PortfolioItem active>Categoria 1</PortfolioItem>
+        <PortfolioItem>Categoria 2</PortfolioItem>
+        <PortfolioItem>Categoria 3</PortfolioItem>
+        <PortfolioItem>Categoria 4</PortfolioItem>
+        <PortfolioItem>Categoria 5</PortfolioItem>
+      </List>
+
+      <div className="box">
+        <Box>
+          <Image src="images/Portfolio/portfolio-img1.jpg" alt="" />
+          <Overlay>
+            <OverlaySpan>Detalhes</OverlaySpan>
+          </Overlay>
+        </Box>
+
+        <Box>
+          <Image src="images/Portfolio/portfolio-img2.jpg" alt="" />
+          <Overlay>
+            <OverlaySpan>Detalhes</OverlaySpan>
+          </Overlay>
+        </Box>
+
+        <Box>
+          <Image src="images/Portfolio/portfolio-img3.jpg" alt="" />
+          <Overlay>
+            <OverlaySpan>Detalhes</OverlaySpan>
+          </Overlay>
+        </Box>
+
+        <Box>
+          <Image src="images/Portfolio/portfolio-img4.jpg" alt="" />
+          <Overlay>
+            <OverlaySpan>Detalhes</OverlaySpan>
+          </Overlay>
+        </Box>
+
+        <Box>
+          <Image src="images/Portfolio/portfolio-img5.jpg" alt="" />
+          <Overlay>
+            <OverlaySpan>Detalhes</OverlaySpan>
+          </Overlay>
+        </Box>
+
+        <Box>
+          <Image src="images/Portfolio/portfolio-img6.jpg" alt="" />
+          <Overlay>
+            <OverlaySpan>Detalhes</OverlaySpan>
+          </Overlay>
+        </Box>
+
+        <Box>
+          <Image src="images/Portfolio/portfolio-img7.jpg" alt="" />
+          <Overlay>
+            <OverlaySpan>Detalhes</OverlaySpan>
+          </Overlay>
+        </Box>
+
+        <Box>
+          <Image src="images/Portfolio/portfolio-img8.jpg" alt="" />
+          <Overlay>
+            <OverlaySpan>Detalhes</OverlaySpan>
+          </Overlay>
+        </Box>
+      </div>
+    </PortfolioSection>
+  );
 };
 
 export default Portfolio;
