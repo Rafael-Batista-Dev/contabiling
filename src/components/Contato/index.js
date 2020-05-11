fec844bdbea774fb7964771a5244cf867bdaa38a
unknown <rafaelbatista.sth@gmail.com>
2020-05-10 21:15:30 -0300

    create page contato

--- src/components/Contato/index.js
+++ src/components/Contato/index.js
@@ -1,7 +1,40 @@
 import React from "react";
+import {
+  ContactSection,
+  ContactTitle,
+  Span,
+  Form,
+  FormInput,
+  InputText,
+  InputEmail,
+  InputExp,
+  TextArea,
+  InputSubmit,
+} from "./style.js";
 
+import Footer from "./../Footer";
 const Contato = () => {
-  return <div>Contato</div>;
+  return (
+    <React.Fragment>
+      <ContactSection>
+        <div class="container">
+          <ContactTitle>
+            <Span>Entre </Span>em contato
+          </ContactTitle>
+          <Form action="">
+            <FormInput>
+              <InputText type="text" placeholder="Nome" />
+              <InputEmail type="email" placeholder=" Email" />
+            </FormInput>
+            <InputExp type="text" class="sub" placeholder="Assunto" />
+            <TextArea cols="30" rows="10" placeholder="Menssagem"></TextArea>
+            <InputSubmit type="submit" value="Enviar" />
+          </Form>
+        </div>
+      </ContactSection>
+      <Footer />
+    </React.Fragment>
+  );
 };
 
 export default Contato;
