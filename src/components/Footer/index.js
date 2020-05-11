6414308d395b57a88371b0f3f730cf7bb943d566
unknown <rafaelbatista.sth@gmail.com>
2020-05-10 21:21:32 -0300

    create section

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { FooterSection, FooterDesc } from "./style.js";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterSection>
+      <FooterDesc>
+        {" "}
+        copyright &copy; 2020 todo os direitos reservados{" "}
+      </FooterDesc>
+    </FooterSection>
+  );
 };
 
 export default Footer;
