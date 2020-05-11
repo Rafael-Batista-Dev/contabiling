298add8ee1bfa1e8edc65f4621321af230556b2c
unknown <rafaelbatista.sth@gmail.com>
2020-05-10 21:03:46 -0300

    create section

--- src/components/RedesSociais/index.js
+++ src/components/RedesSociais/index.js
@@ -1,7 +1,43 @@
 import React from "react";
+import {
+  SocialSection,
+  Social,
+  Icon,
+  SocialDesc,
+  Span,
+  SpanInfo,
+} from "./style.js";
 
-const RedesSociais = () => {
-  return <div>RedesSociais</div>;
-};
+import axios from "axios";
 
-export default RedesSociais;
+class SocailMedia extends React.Component {
+  state = {
+    social: [],
+  };
+
+  componentDidMount() {
+    axios.get("js/data.json").then((res) => {
+      this.setState({ social: res.data.social });
+    });
+  }
+
+  render() {
+    const { social } = this.state;
+
+    const socialList = social.map((socialItem) => {
+      return (
+        <Social item={socialItem.id} key={socialItem.id}>
+          <Icon className={socialItem.icon}></Icon>
+          <SocialDesc>
+            <Span>{socialItem.title}</Span>
+            <SpanInfo>{socialItem.body}</SpanInfo>
+          </SocialDesc>
+        </Social>
+      );
+    });
+
+    return <SocialSection>{socialList}</SocialSection>;
+  }
+}
+
+export default SocailMedia;
